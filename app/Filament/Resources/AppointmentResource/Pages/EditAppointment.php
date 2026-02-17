00d389067649b6efa2dd4e7aff611325b28f504d
Verónica Guzmán Ochoa <heylistennavi@proton.me>
2026-02-17 00:59:02 -0800

    update: make retellai api call send variables too

--- app/Filament/Resources/AppointmentResource/Pages/EditAppointment.php
+++ app/Filament/Resources/AppointmentResource/Pages/EditAppointment.php
@@ -24,9 +24,19 @@ class EditAppointment extends EditRecord
                 ->label('Avisar Cita')
                 ->icon('heroicon-o-bell-alert')
                 ->color('info')
+                ->form([
+                    Forms\Components\TextInput::make('doctor_name')
+                        ->label('Nombre del Doctor')
+                        ->required()
+                        ->placeholder('Dr. Ejemplo'),
+                    Forms\Components\TextInput::make('practice')
+                        ->label('Nombre del consultorio')
+                        ->required()
+                        ->placeholder('Asistente Ejemplo'),
+                ])
                 ->action(function (Appointment $record, array $data) {
                     $apiKey = env('RETELL_API_KEY');
-                    $agentId = 'agent_cbb6275c270ab16a3f6036de49';
+                    $agentId = 'agent_b38569dae01a294e4655094525';
                     $fromNumber = '+18456066291';
 
                     $toNumber = $record->chat_id;
@@ -37,6 +47,14 @@ class EditAppointment extends EditRecord
                                 'from_number' => $fromNumber,
                                 'to_number'   => $toNumber,
                                 'override_agent_id' => $agentId,
+
+                                'retell_llm_dynamic_variables' => [
+                                    'nombre_paciente' => $record->patient_name,
+                                    'fecha_cita_iso'      => $record->appointment_date,
+                                    'hora_cita_iso'       => $record->appointment_time,
+                                    'nombre_doctor'   => $data['doctor_name'],
+                                    'nombre_consultorio' => $data['practice'],
+                                ]
                             ]);
 
                         if ($response->successful()) {
@@ -51,8 +69,10 @@ class EditAppointment extends EditRecord
 
                             return;
                         }
+
                         Notification::make()
                             ->title('Error al iniciar la llamada')
+                            ->body('Código de error: ' . $response->body())
                             ->danger()
                             ->send();
 
