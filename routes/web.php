67f123fa84d526a759f657231926d6de35488288
Verónica Guzmán Ochoa <heylistennavi@proton.me>
2026-03-30 03:37:02 -0700

    fix: delete quotes and welcome views and add a redirect

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\InvoiceController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->away('https://hurtadoplasticsurgery.com');
 });
-
-Route::get("/generate-quote/pdf/{quote}", [InvoiceController::class, "generatePDF"])
-    ->name("quote.generate.pdf");
-
-Route::get("/generate-quote/html/{quote}", [InvoiceController::class, "generateHTML"])
-    ->name("quote.generate.html");
